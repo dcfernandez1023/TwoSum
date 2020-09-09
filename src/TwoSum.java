83c8b5384c14822144f5c0fd3ba0d367ce59c0fc
Dominic Fernandez <dominic.fernandez1023@gmail.com>
2020-09-08 22:56:12 -0700

    accepts user inputs now

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,29 +1,85 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
 
 class TwoSum {
 
-    public int[] unsortedTwoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        for(int i = 0; i < nums.length; i++) {
-            int index1 = i;
-            for(int x = 0; x < nums.length; x++) {
-                int index2 = x;
-                if(nums[index1] + nums[index2] == target && index1 != index2) {
-                    result[0] = index1;
-                    result[1] = index2;
+    public ArrayList<Integer> twoSum(List<Integer> nums, int target) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        System.out.println(result.size());
+        for(int i = 0; i < nums.size(); i++) {
+            for(int x = 0; x < nums.size(); x++) {
+                if(nums.get(i) + nums.get(x) == target && i != x) {
+                    result.add(i);
+                    result.add(x);
                     return result;
                 }
             }
         }
-        result[0] = -1;
-        result[1] = -1;
+        result.add(-1);
+        result.add(-1);
         return result;
     }
 
+    private ArrayList<Integer> getArrayInputs() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        boolean isInputting = true;
+        int i = 0;
+        while(isInputting) {
+            System.out.println("Enter a number to be added to the array, or 'd' to use the existing array: ");
+            System.out.println("Current array: " + arr.toString());
+            String input = scanner.nextLine();
+            if(input.equals("d")) {
+                isInputting = false;
+            }
+            else if(this.isInt(input)) {
+                arr.add(i, Integer.parseInt(input));
+                i++;
+            }
+            else {
+                System.out.println("'" + input + "' " + "is not a number. Please enter a number.");
+            }
+        }
+        return arr;
+    }
+
+    private int getTarget() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isInputting = true;
+        int target = -1;
+        while(isInputting) {
+            System.out.println("Enter the Two Sum target value: ");
+            String input = scanner.nextLine();
+            if(this.isInt(input)) {
+                isInputting = false;
+                target = Integer.parseInt(input);
+            }
+            else {
+                System.out.println("'" + input + "' " + "is not a number. Please enter a number.");
+            }
+        }
+        return target;
+    }
+
+    private boolean isInt(String input) {
+        if(input == null) {
+            return false;
+        }
+        try {
+            Integer.parseInt(input);
+        }
+        catch(NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String args[]) {
-        int[] testArr = {1,6,2,3,4,0};
-        int target = 12;
         TwoSum twoSum = new TwoSum();
-        System.out.println(Arrays.toString(twoSum.unsortedTwoSum(testArr, target)));
+        System.out.println("~~ TWO SUM ~~");
+        int target = twoSum.getTarget();
+        ArrayList<Integer> arr = twoSum.getArrayInputs();
+        System.out.println("Result " + twoSum.twoSum(arr, target));
     }
 }
